ea395ccd08a1fe6858acba43dc5b1ffd19f6c614
daviek19-homepc <daviek19@gmail.com>
2017-05-09 21:03:19 +0300

    working on workshop

---
+++ application/models/workshop/Jobcards_model.php
@@ -0,0 +1,14 @@
+<?php
+
+class Jobcards_model extends CI_Model
+{
+
+    private $workshop_db;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+        $this->workshop_db = $this->load->database('workshop', true);
+    }
+}
