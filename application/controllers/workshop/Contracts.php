a8db6f045ceb0bfb0cdb8f2c9f5b7938a70edf1d
blackhawk <daviek19@gmail.com>
2017-06-12 21:32:36 +0300

    working on contracts

--- application/controllers/workshop/Contracts.php
+++ application/controllers/workshop/Contracts.php
@@ -12,4 +12,138 @@ class Contracts extends REST_Controller
         parent::__construct();
         $this->load->model('workshop/contracts_model');
     }
+
+    public function index_get()
+    {
+        //Get params
+        $company_id = (int)$this->get('company_id');
+
+        log_message("debug", "*********** index_get start company_id {$company_id} ***********");
+
+        $result = $this->contracts_model->get_all_contracts($company_id);
+
+        $this->response([
+            'response' => $result,
+            'status' => TRUE,
+            'description' => 'To get all [/workshop/Contractors/company_id/] or to get single [/workshop/Contractors/company_id/item_id]'
+        ], REST_Controller::HTTP_OK);
+
+    }
+
+    public function find_get()
+    {
+        $contract_id = (int)$this->get('contract_id');
+
+        $result = $this->contracts_model->get_single_contract("", $contract_id);
+
+        $this->response([
+            'response' => $result,
+            'status' => TRUE,
+            'description' => 'To get all [/workshop/contractors/company_id/] or to get single [/workshop/contractors/company_id/bank_id]'
+        ], REST_Controller::HTTP_OK);
+    }
+
+    public function index_put()
+    {
+        $data = array(
+            'company_id' => $this->put('company_id'),
+            'contractor_name' => $this->put('contractor_name'),
+            'id_no' => $this->put('id_no'),
+            'pin_no' => $this->put('pin_no'),
+            'mobile_no' => $this->put('mobile_no'),
+            'section_id' => $this->put('section_id'),
+            'location_id' => $this->put('location_id'),
+            'bank_id' => $this->put('bank_id'),
+            'branch_code' => $this->put('branch_code'),
+            'account_no' => $this->put('account_no'),
+            'active' => $this->put('active'),
+            'user_id' => $this->put('user_id')
+        );
+
+        log_message("debug", "Getting ready to insert... " . json_encode($data));
+
+        if (empty($data['company_id'])) {
+
+            return $this->response([
+                'status' => FALSE,
+                'message' => 'Trying to create empty company id',
+                'description' => ''
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+        if (empty($data['contractor_name'])) {
+
+            return $this->response([
+                'status' => FALSE,
+                'message' => 'Trying to create with empty contractor name',
+                'description' => ''
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+        if (empty($data['id_no'])) {
+            return $this->response([
+                'status' => FALSE,
+                'message' => 'Trying to create with empty Id no',
+                'description' => ''
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+        if (empty($data['mobile_no'])) {
+            return $this->response([
+                'status' => FALSE,
+                'message' => 'Trying to create with empty Mobile No',
+                'description' => ''
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+        if (empty($data['section_id'])) {
+            return $this->response([
+                'status' => FALSE,
+                'message' => 'Trying to create with empty section',
+                'description' => ''
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+        if (empty($data['location_id'])) {
+            return $this->response([
+                'status' => FALSE,
+                'message' => 'Trying to create with empty location',
+                'description' => ''
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+        if (empty($data['bank_id'])) {
+            return $this->response([
+                'status' => FALSE,
+                'message' => 'Trying to create with empty bank details',
+                'description' => ''
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+
+        if ($this->contractors_model->contractor_exists($data['contractor_name'], $data['company_id']) == TRUE) {
+
+            return $this->response([
+                'response' => $data,
+                'status' => FALSE,
+                'message' => 'Trying to duplicate a Contractor ',
+                'description' => ''
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+
+        $response = $this->contractors_model->create_contractor($data);
+
+        if ($response == FALSE) {
+
+            log_message("debug", "index_put Database refused. Try again!... ");
+
+            return $this->response([
+                'response' => $data,
+                'status' => FALSE,
+                'message' => 'Database refused. Try again!',
+                'description' => 'create group put/ {company_id,warehouse_name,wh_loc_id} name cannot be null'
+            ], REST_Controller::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return $this->response([
+            'response' => $response,
+            'status' => true,
+            'message' => 'Contractor created!',
+            'description' => ''
+        ], REST_Controller::HTTP_CREATED);
+    }
+
 }
--- application/models/workshop/Contracts_model.php
+++ application/models/workshop/Contracts_model.php
@@ -62,4 +62,112 @@ FROM
             return false;
         }
     }
+
+    public function get_single_contract($company_id = '0', $contract_id)
+    {
+        if (!empty($contract_id)) {
+
+            $select_query =
+                "SELECT
+    `hd_contracts`.`contract_id`
+    , `hd_contracts`.`job_id`
+    , `hd_contracts`.`company_id`
+    , `hd_contracts`.`contract_date`
+    , `hd_contracts`.`start_date`
+    , `hd_contracts`.`end_date`
+    , `hd_contracts`.`amount`
+    , `hd_contracts`.`contractor_id`
+    , `hd_contracts`.`date_created`
+    , `hd_contracts`.`cust_id`
+    , `customer_vehicle`.`chassis_no`
+    , `customer`.`customer_name`
+    , `contractors`.`contractor_name`
+    , `job_card`.`job_date`
+    , `job_card`.`reg_no`
+    , `customer_vehicle`.`engine_no`
+    , `process`.`process_name`
+    , `process`.`std_days`
+
+FROM
+    `workshop`.`hd_contracts`
+    INNER JOIN `workshop`.`job_card`
+        ON (`hd_contracts`.`job_id` = `job_card`.`job_id`)
+    INNER JOIN `workshop`.`customer`
+        ON (`hd_contracts`.`cust_id` = `customer`.`customer_id`)
+    INNER JOIN `workshop`.`contractors`
+        ON (`hd_contracts`.`contractor_id` = `contractors`.`contractor_id`)
+    INNER JOIN `workshop`.`process`
+        ON (`hd_contracts`.`process_id` = `process`.`process_id`)
+    INNER JOIN `workshop`.`customer_vehicle`
+        ON (`job_card`.`customer_vehicle_id` = `customer_vehicle`.`customer_vehicle_id`)
+         WHERE `hd_contracts`.`contract_id` = {$contract_id};";
+
+            if ($query = $this->workshop_db->query($select_query)) {
+
+                log_message("debug", $this->workshop_db->last_query());
+
+                log_message("debug", "found contract..." . json_encode($query->result()));
+
+                return $query->result();
+            } else {
+
+                log_message("error", 'Error getting contract.');
+
+                return false;
+            }
+        } else {
+            //The vehicle_id was empty
+            return FALSE;
+        }
+    }
+
+    public function create_contract($data)
+    {
+        log_message("debug", "create_contract...data " . json_encode($data));
+
+        if ($this->workshop_db->insert('hd_contracts', $data)) {
+
+            log_message("debug", "contracts query " . $this->workshop_db->last_query());
+
+            $id = $this->workshop_db->insert_id();
+
+            $new_record = $this->workshop_db->get_where('hd_contracts', array('contract_id' => $id));
+
+            log_message("debug", " contractor created " . json_encode($new_record->row()));
+
+            return $new_record->row();
+        } else {
+            return FALSE;
+        }
+    }
+
+    public function update_contract($data)
+    {
+
+        log_message("debug", "Getting ready to update_contracts... " . json_encode($data));
+
+        if (empty($data['contract_id'])) {
+
+            log_message("debug", " contract id was empty. Exit");
+
+            return FALSE;
+        }
+
+        $this->workshop_db->where('contract_id', $data['contract_id']);
+
+        if ($this->workshop_db->update('contracts', $data) == FALSE) {
+
+            return FALSE;
+        }
+        log_message("debug", "update_contracts " . $this->workshop_db->last_query());
+        //All went well
+        $new_record = $this->workshop_db->get_where('contracts', array('contract_id' => $data['contract_id']));
+
+        log_message("debug", " update_contract query " . $this->workshop_db->last_query());
+
+        log_message("debug", "Contract Updated " . json_encode($new_record->row()));
+
+        return $new_record->row();
+    }
+
 }
