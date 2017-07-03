7ad1e87c528f5430b2b8131efb04aff361b9b9e4
David Mwangi <David Mwangi>
2017-07-03 11:40:33 +0300

    still working on the requisitions

--- application/controllers/workshop/Requisitions.php
+++ application/controllers/workshop/Requisitions.php
@@ -30,6 +30,108 @@ class Requisitions extends REST_Controller
         ], REST_Controller::HTTP_OK);
 
     }
+	
+	public function index_put()
+    {
+		 $data = array(           
+            'company_id' => $this->put('company_id'),
+			'req_date' => $this->put('req_date'),
+            'job_no' => $this->put('job_no'),
+            'job_date' => $this->put('job_date'),
+            'requested_by' => $this->put('requested_by'),
+            'section_id' => $this->put('section_id'),
+            'chassis_no' => $this->put('chassis_no'),
+			'status' => $this->put('status'),
+			'cancel' => $this->put('cancel'),			
+            'user_id' =>  $this->put('user_id')
+        );
+		
+        log_message("debug", "Getting ready to insert... " . json_encode($data));
+
+        if (empty($data['req_date'])) {
+          
+            return $this->response([
+                'status' => FALSE,
+                'message' => 'the requisition date is required',
+                'description' => ''
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+		
+        if (empty($data['job_no'])) {
+
+            return $this->response([
+                'status' => FALSE,
+                'message' => 'The job number is required',
+                'description' => ''
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+     
+    	if (empty($data['job_date'])) {
+            return $this->response([
+                'status' => FALSE,
+                'message' => 'The job data is required',
+                'description' => ''
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+       
+   	    if (empty($data['requested_by'])) {
+            return $this->response([
+                'status' => FALSE,
+                'message' => 'Requested by field by is required.',
+                'description' => ''
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+		
+        if (empty($data['section_id'])) {
+            return $this->response([
+                'status' => FALSE,
+                'message' => 'Select a valid section',
+                'description' => ''
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+		
+        if (empty($data['chassis_no'])) {
+            return $this->response([
+                'status' => FALSE,
+                'message' => 'chassis no is required.',
+                'description' => ''
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+		       
+        $response = $this->requisitions_model->create_requisation($data);
+
+        if ($response == FALSE) {
+
+            log_message("debug", "index_put Database refused. Try again!... ");
+
+            return $this->response([
+                'response' => $data,
+                'status' => FALSE,
+                'message' => 'Database refused. Try again!',
+                'description' => 'create group put/ {company_id,warehouse_name,wh_loc_id} name cannot be null'
+            ], REST_Controller::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return $this->response([
+            'response' => $response,
+            'status' => true,
+            'message' => 'requisition created!',
+            'description' => ''
+        ], REST_Controller::HTTP_CREATED);
+    }
+
+	 public function find_get()
+    {
+        $requisition_id = (int)$this->get('requisition_id');
+
+        $result = $this->requisitions_model->get_single_requisations("", $requisition_id);
+
+        $this->response([
+            'response' => $result,
+            'status' => TRUE,
+            'description' => 'To get all [/workshop/vehicles/company_id/] or to get single [/workshop/vehicles/company_id/item_id]'
+        ], REST_Controller::HTTP_OK);
+    }
 
 }
 
--- application/models/workshop/Requisitions_model.php
+++ application/models/workshop/Requisitions_model.php
@@ -65,4 +65,86 @@ FROM
         }
     }
 
+	public function create_requisation($data)
+    {
+      
+        log_message("debug", "create_requisation...data " . json_encode($data));   
+        
+        if ($this->workshop_db->insert('requisition', $data)) {
+
+            log_message("debug", "requisition query " . $this->workshop_db->last_query());
+
+            $id = $this->workshop_db->insert_id();
+
+            $new_record = $this->workshop_db->get_where('requisition', array('req_id' => $id));
+
+            log_message("debug", " requisition created " . json_encode($new_record->row()));
+
+            return $new_record->row();
+        } else {
+            return FALSE;
+        }
+    }
+
+  public function get_single_requisations($company_id = '0',$requisition_id)
+    {
+		 if (!empty($requisition_id)) {
+			 
+        $select_query =
+            "SELECT
+    `requisition`.`company_id`
+    ,`requisition`.`req_id`
+    ,`requisition`.`req_date`
+    ,`requisition`.`job_no`
+    ,`requisition`.`job_date`
+    ,`requisition`.`requested_by`
+    ,`requisition`.`section_id`
+    ,`requisition`.`chassis_no`
+    ,`requisition`.`status`
+    ,`requisition`.`cancel`
+     ,`requisition`.`date_created`
+    , `parameter_description`.`description_id`
+    , `parameter_description`.`description_name`
+    , `contractors`.`contractor_id`
+    , `contractors`.`contractor_name`
+    , `customer`.`customer_id`
+    , `customer`.`customer_name`
+    , `customer_vehicle`.`chassis_no`
+    , `customer_vehicle`.`engine_no`
+    , `job_card`.`job_id`
+FROM
+    `workshop`.`requisition`
+    INNER JOIN `workshop`.`job_card`
+        ON (`requisition`.`job_no` = `job_card`.`job_id`)
+    INNER JOIN `workshop`.`contractors`
+        ON (`requisition`.`requested_by` = `contractors`.`contractor_id`)
+    INNER JOIN `workshop`.`parameter_description`
+        ON (`requisition`.`section_id` = `parameter_description`.`description_id`)
+    INNER JOIN `workshop`.`customer_vehicle`
+        ON (`job_card`.`customer_vehicle_id` = `customer_vehicle`.`customer_vehicle_id`)
+    INNER JOIN `workshop`.`customer`
+        ON (`customer_vehicle`.`customer_id` = `customer`.`customer_id`)
+WHERE `requisition`.`req_id` = {$requisition_id};";
+		
+
+         if ($query = $this->workshop_db->query($select_query)) {
+
+                log_message("debug", $this->workshop_db->last_query());
+
+                log_message("debug", "found requisition..." . json_encode($query->result()));
+
+                return $query->result();
+            } else {
+
+                log_message("error", 'Error getting contract.');
+
+                return false;
+            }
+        } else {
+            //The vehicle_id was empty
+            return FALSE;
+        }
+	
+    }
+
 }
