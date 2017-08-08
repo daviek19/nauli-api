0f455a93d95fafb5f2ee970701a1339cc946d4af
david vps <daviek19@gmail.com>
2017-08-08 10:31:14 +0300

    these are changes from live envirnoment

--- application/controllers/workshop/Closejobs.php
+++ application/controllers/workshop/Closejobs.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 require APPPATH . '/libraries/REST_Controller.php';
 
-class CloseJobs extends REST_Controller
+class Closejobs extends REST_Controller
 {
     function __construct()
     {
